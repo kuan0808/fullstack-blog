1566400eaecc87ce304fea86bdc3d1adb5f04bf1
Kuan Ting Pan <kuanthedev@gmail.com>
2022-03-09 18:19:38 +0800

    Categories component finished

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,13 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
-const categories = [
-  { name: "React", slug: "react" },
-  { name: "Tailwind", slug: "tailwind" },
-];
+import { getCategories } from "../services";
 
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    getCategories().then((result) => setCategories(result));
+  }, []);
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
