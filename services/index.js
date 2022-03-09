b43ce4ac8d194310d86f79849bacee4cef6a4bdd
Kuan Ting Pan <kuanthedev@gmail.com>
2022-03-10 02:23:09 +0800

    comments features finished

--- services/index.js
+++ services/index.js
@@ -121,3 +121,29 @@ export const getCategories = async () => {
   const result = await request(graphqlAPI, query);
   return result.categories;
 };
+
+export const submitComment = async (comment) => {
+  const result = await fetch("/api/comments", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(comment),
+  });
+
+  return result.json();
+};
+
+export const getComments = async (slug) => {
+  const query = gql`
+    query GetComments($slug: String!) {
+      comments(where: { post: { slug: $slug } }) {
+        name
+        createdAt
+        comment
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query, { slug });
+  return result.comments;
+};
